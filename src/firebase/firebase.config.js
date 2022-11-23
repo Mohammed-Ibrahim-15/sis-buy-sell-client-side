fa1efd96c8c0d921e08d1a3773bc8f53e9393a47
Mohammed-Ibrahim-15 <ibrahim72342@gmail.com>
2022-11-23 23:06:35 +0600

    firebase added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCYPQ6ajUEJzFXRCdHfoj-clzEEFVFnxps",
+    authDomain: "sis-buy-sell-client-side.firebaseapp.com",
+    projectId: "sis-buy-sell-client-side",
+    storageBucket: "sis-buy-sell-client-side.appspot.com",
+    messagingSenderId: "441626827778",
+    appId: "1:441626827778:web:0513e9dbc479dd72671b1b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
