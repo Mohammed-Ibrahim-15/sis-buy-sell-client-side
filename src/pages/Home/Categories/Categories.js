613afbd29170aa24b377e90989206bcba50c283d
Mohammed-Ibrahim-15 <ibrahim72342@gmail.com>
2022-11-24 20:45:05 +0600

    category details added

--- src/pages/Home/Categories/Categories.js
+++ src/pages/Home/Categories/Categories.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import CategoriesDetails from '../CategoryDetails/CategoriesDetails';
 
-const Categories = ({ name }) => {
+const Categories = () => {
     const category = useLoaderData()
     // console.log(category)
     return (
-        <div>
-            <h1>All Phones</h1>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+        <div className='max-w-screen-xl mx-auto'>
+            <h1 className='text-center text-4xl font-bold mt-4'>All Phones</h1>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-12'>
                 {
                     category.map(cat => <CategoriesDetails
                         key={cat._id}
--- src/pages/Home/CategoryDetails/CategoriesDetails.js
+++ src/pages/Home/CategoryDetails/CategoriesDetails.js
@@ -1,17 +1,26 @@
 import React from 'react';
 
 const CategoriesDetails = ({ cat }) => {
-    const { name, img, location, condition } = cat
+    const { name, img, location, condition, description, mobile, month_of_use, original_price, purchase_year, resale_price, seller_name, post_time } = cat
     console.log(cat)
     return (
         <div>
             <div className="card w-96 bg-base-100 shadow-xl">
                 <figure className="px-10 pt-10">
-                    <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl" />
+                    <img src={img} alt="Mobile" className="rounded-xl w-60 h-60" />
                 </figure>
                 <div className="card-body items-center text-center">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <h2 className="card-title">{name}</h2>
+                    <p>Description: {description} </p>
+                    <p>Condition: {condition} </p>
+                    <p>Location {location} </p>
+                    <p>Month of Use: {month_of_use} /month </p>
+                    <p>Purchase Year: {purchase_year}</p>
+                    <p>Selling Price: {resale_price} </p>
+                    <p>Buying Price: {original_price} </p>
+                    <p>Seller Name: {seller_name} </p>
+                    <p>Contact No: {mobile} </p>
+                    <p>Post On: {post_time} </p>
                     <div className="card-actions">
                         <button className="btn btn-primary">Buy Now</button>
                     </div>
